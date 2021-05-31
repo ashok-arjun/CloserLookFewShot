0a602fb0442cc2009baa37bbf51f2546bc2c1b41
priyamdey <pd11@iitbbs.ac.in>
2021-05-31 12:35:44 +0530

    minor - made miniImageNet work on machine

--- train.py
+++ train.py
@@ -156,6 +156,7 @@ if __name__=='__main__':
     model = model.cuda()
 
     params.checkpoint_dir = '%s/checkpoints/%s/%s_%s' %(configs.save_dir, params.dataset, params.model, params.method)
+    print(params.checkpoint_dir)
     if params.train_aug:
         params.checkpoint_dir += '_aug'
     if not params.method  in ['baseline', 'baseline++']: 
